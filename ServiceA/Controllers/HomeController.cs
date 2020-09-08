144e6bd3e55a2212827353281c053df6081a301a
xuj@das.com <xuj@das.com>
2020-09-08 09:44:58 +0800

    添加项目文件。

---
+++ ServiceA/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ServiceA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("访问服务");
+            _logger.LogWarning("出现警告");
+            return "From ServiceA";
+        }
+    }
+}
